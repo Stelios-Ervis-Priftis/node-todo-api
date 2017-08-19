1c210026ffb0f5d1354242a27d97685b21d89e68
Ervis-Priftis <steliospriftis@gmail.com>
2017-08-19 21:00:12 +0100

    Add POST /todos route and refactor mongoose

---
+++ server/models/user.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+
+var User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1
+  }
+});
+
+
+// var newUser = new User({
+//   email: 'ervis@example.com'
+// });
+//
+// newUser.save().then((doc) => {
+//   console.log(JSON.stringify(doc, undefined, 2));
+// }, (err) => {
+//   console.log('Unable to save user', err);
+// });
+
+module.exports = {User};
