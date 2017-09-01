ccbc2e74f976dc1a64ab7354ac0f1c6929d356db
Ervis-Priftis <steliospriftis@gmail.com>
2017-09-01 22:07:38 +0300

    Add some comment to DELETE /todos route

--- server/server.js
+++ server/server.js
@@ -51,19 +51,26 @@ app.get('/todos/:id', (req, res) => {
 });
 
 app.delete('/todos/:id', (req, res) => {
+  // get the id
   var id = req.params.id;
 
+  //  validate the id -> not valid? return 404
   if(!ObjectID.isValid(id)){
     return res.status(404).send('ID IS NOT VALID');
   }
 
+  // return todo by id
   Todo.findByIdAndRemove(id).then((todo) => {
+    // if no doc, send 404
     if (!todo) {
       return res.status(404).send('TODOS ID NOT FOUND');
     } else {
+      // if doc, send doc back with 200
       res.send({todo});
     }
+    // error
   }).catch((e) => {
+    // 400 with empty body
     res.status(400).send();
   });
 });
